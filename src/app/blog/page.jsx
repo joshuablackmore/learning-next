614aaff2cc2986705975ca39aca74d7616a6bed3
joshuablackmore <106779167+joshuablackmore@users.noreply.github.com>
2023-06-16 10:39:45 +0100

    trying out orientation change on nav

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -21,7 +21,7 @@ const Blog = async () => {
      <div className='grid grid-cols-4 m-50 gap-2 text-black pt-20 mx-5  '>
      {data.items.map((post) => (
       
-        <div className='flex col-span-2 flex flex-col items-center justify-center text-center border rounded-md border-slate-400 shadow-lg shadow-slate-600 p-9 overflow-hidden' key={post.id}>
+        <div className='flex col-span-2 flex-col items-center justify-center text-center border rounded-md border-slate-400 shadow-lg shadow-slate-600 p-9 overflow-hidden' key={post.id}>
           <h3 className='font-bold'>{post.title}</h3>
           <h2 className=''>{post.id}</h2>
           <div dangerouslySetInnerHTML={{ __html: post.content }} />
