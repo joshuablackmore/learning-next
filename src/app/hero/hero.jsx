c08af03e24e8023b97877361f77cb2db546a3e42
joshuablackmore <106779167+joshuablackmore@users.noreply.github.com>
2023-06-27 10:12:07 +0100

    added some landscape classes

--- src/app/hero/hero.jsx
+++ src/app/hero/hero.jsx
@@ -21,9 +21,9 @@ const Hero = () => {
     
     
   return (
-    <div className='flex items-center flex-col h-screen bg-light1 box-border overflow-y-auto'>
+    <div className='flex items-center flex-col h-screen bg-light1 box-border landscape:flex-row '>
 
-        <div className='min-h-[395px] h-[50%] w-[100%] md:h-[60%] lg:h-[90%] xl:h-[100%] max-w-[1180px] flex items-center'>
+        <div className='min-h-[395px] h-[50%] w-[100%] md:h-[60%] lg:h-[90%] xl:h-[100%] max-w-[1180px] flex items-center landscape:border '>
             {imageLoaded && (
             <motion.div
             initial={{ x: -1000, opacity: 0 }}
@@ -31,20 +31,20 @@ const Hero = () => {
             transition={{ duration: 0.8 }}
             className='flex justify-center h-full w-full bg-cover'
             >
-                <img src={imageSrc} className='flex object-cover h-full'></img>
+                <img src={imageSrc} className='flex object-cover h-full '></img>
                 </motion.div>
             )}
             </div>
             
         
 
-            <div className='h-[50%] w-[100%] flex items-center'>
+            <div className='h-[50%] w-[100%] flex items-center landscape:overflow-scroll landscape:h-full'>
                 {imageLoaded && (
                 <motion.div
                 initial={{ opacity: 0}}
                 animate={{ opacity: 1}}
                 transition={{ delay: 0.8, duration: 1.5 }}
-                className=' h-full w-full flex items-start mt-2 md:mx-2 xl:mx-20 '>
+                className=' h-full w-full flex items-start mt-2 md:mx-2 xl:mx-20  '>
                 <div className=' m-2 mt-6'>
                     <h1 className='  text-2xl'>I paint and draw things</h1>
                     <p className=' pt-6 py-2'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. In iure quae minus! Corporis est enim nulla? Totam, labore ipsam! Corporis est ullam facere veritatis sit perspiciatis nulla ea earum hic?</p>
