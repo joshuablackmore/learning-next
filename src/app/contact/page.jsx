89756f3399edc219e683c9d574efed61ae77baf7
joshuablackmore <106779167+joshuablackmore@users.noreply.github.com>
2023-07-18 22:54:18 +0100

    .

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,36 +1,10 @@
 import React from 'react'
-import { getArtwork } from '../../../sanity/schemas/sanity-utils'
-import Image from 'next/image';
 
-export default async function Contact() {
-  const artwork = await getArtwork();
+export default function Contact() {
+  
 
-  console.log(artwork)
 
-  return (
-    <div className='h-screen pt-[45px]'>
-      <div>
-        {artwork.map((pic) => (
-          <div><Image 
-          src={pic.image}
-          id='id'
-          width={250}
-          height={250}/>
-          <h1>{pic._id}</h1>
-          <h2>{pic.name}</h2>
-          </div>
-        ))}
-      </div>
-      
-      <div>
-        <Image 
-        src={artwork[1].image}
-        width={250}
-        height={250} />
-        <h1>{artwork[1].name}</h1>
-      </div>
 
-    </div>
-  )
+  return <div className='pt-[45px]'>contact</div>
 }
 
--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -11,7 +11,7 @@ export default async function Portfolio() {
     <div className='min-h-screen bg-light2 flex flex-col pt-2 gap-2 sm:grid sm:grid-cols-1 sm:grid-rows-1 md:grid-cols-3 md:grid-rows-3'>
     <h1 className="flex justify-center items-center font-bold text-2xl">Portfolio</h1>
     {data.map((imgs) => (
-        <div className="flex justify-center items-center">
+        <div key={imgs._id} className="flex justify-center items-center">
         <ClientPortfolio id={imgs._id} name={imgs.name} image={imgs.image} alt={imgs.alt} />
         </div>
     ))}
