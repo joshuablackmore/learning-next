d0376f03a22e183249ba73864a22766ebeed1528
Joshua Blackmore <joshuablackmore@gmail.com>
2024-11-20 14:02:47 +0000

    removed broken insta

--- src/app/components/NavBar/links.ts
+++ src/app/components/NavBar/links.ts
@@ -1,11 +1,10 @@
 export const navigationLinks = [
   { path: "/", label: "Home" },
   { path: "/#portfolio", label: "Portfolio" },
-  { path: "/#insta", label: "Instagram" },
+  // { path: "/#insta", label: "Instagram" },
   { path: "/#contact", label: "Contact" },
-  { path: "/blog", label: "Blog" },
+  // { path: "/blog", label: "Blog" },
   // {
-  //   path: "https://www.instagram.com/petedblackmore",
-  //   label: <AiFillInstagram size={30} />,
+  // {path: "https://www.instagram.com/petedblackmore", label: <AiFillInstagram size={ 30} /> },
   // },
 ] as const;
--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
     <main>
       <HeroServer />
       <Portfolio />
-      <Gallery />
+      {/* <Gallery /> */}
       <ContactForm />
     </main>
   );
